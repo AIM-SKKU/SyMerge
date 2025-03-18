5cb5e21911ab44861aebea59064b42603a15f365
kasurashan <73826816+kasurashan@users.noreply.github.com>
2025-03-18 15:13:49 +0900

    Update train_plus.sh

--- src/train_plus.sh
+++ src/train_plus.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH="/root/workspace/arithmetic/ModelTinting/src"
-CUDA_VISIBLE_DEVICES=0 python /root/workspace/arithmetic/AdaMerging/src/main_layer_wise_adamerging.py \
+CUDA_VISIBLE_DEVICES=0 python /root/workspace/arithmetic/ModelTinting/src/main_layer_wise_adamerging.py \
     --losstype crosspseudoconf \
     --model ViT-B-32 \
     --prior 0.3 \
