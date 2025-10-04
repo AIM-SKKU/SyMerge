87c937ad1dadd0368a5bbb97bb09b5d4d26eb5ed
kasurashan <73826816+kasurashan@users.noreply.github.com>
2025-10-05 03:26:03 +0900

    Delete src directory

--- src/train.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH="/root/workspace/arithmetic/ModelTinting/src"
-CUDA_VISIBLE_DEVICES=0 python /root/workspace/arithmetic/AdaMerging/src/main_layer_wise_adamerging.py \
-    --losstype crosspseudo \
-    --model ViT-B-32 \
-    --prior 0.3 \
-    --classifier_train
--- src/train_plus.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH="/root/workspace/arithmetic/ModelTinting/src"
-CUDA_VISIBLE_DEVICES=0 python /root/workspace/arithmetic/ModelTinting/src/main_layer_wise_adamerging.py \
-    --losstype crosspseudoconf \
-    --model ViT-B-32 \
-    --prior 0.3 \
-    --classifier_train
